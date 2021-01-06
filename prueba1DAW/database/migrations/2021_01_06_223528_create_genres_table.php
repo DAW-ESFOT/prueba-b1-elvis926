e05332491a7cb3cd47ebf774b290755807be13bd
DESKTOP-SMED30I\cocho <elvis.agila@epn.edu.ec>
2021-01-06 18:12:03 -0500

    Creacion de modelos y migraciones

--- prueba1DAW/database/migrations/2021_01_06_223528_create_genres_table.php
+++ prueba1DAW/database/migrations/2021_01_06_223528_create_genres_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateGenresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('genres', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name',255);
-            $table->string('lastname',255);
-            $table->string('document',80);
-            $table->rememberToken();
-            $table->timestamps();
-
+            $table->string('name');
         });
     }
 
@@ -31,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('genres');
     }
 }
